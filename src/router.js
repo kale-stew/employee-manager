b384e5bb3c873414d1fa54672514a90cd8d1ca0a
Kylie Stewart <kylie.stewart@me.com>
2017-12-09 16:02:54 -0700

    main vs auth flow

--- src/router.js
+++ src/router.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root">
-                <Scene 
-                    key="login"
-                    component={LoginForm}
-                    title="Please Login"
-                />
+                <Scene key="auth" hideNavBar={true}>
+                    <Scene key="login" component={LoginForm} title="Please Login" hideNavBar={false} />
+                </Scene>
+
+                <Scene key="main" hideNavBar={true}>
+                    <Scene 
+                        key="employeeList" 
+                        component={EmployeeList} 
+                        title="Employees" 
+                        hideNavBar={false} 
+                        rightTitle="Add New"
+                        onRight={ () => Actions.employeeCreate() }
+                    />
+                    <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" hideNavBar={false}/>
+                </Scene>
             </Scene>
         </Router>
     )
