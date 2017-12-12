397c0c8d685477cc4bf48e6917b9f7b0cad4888d
Kylie Stewart <kylie.stewart@me.com>
2017-12-11 18:35:10 -0700

    Functional edit vs create scenes

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,22 +1,39 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }
+
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+        console.log(name, phone, shift);
+    }
+
     render() {
         return (
             <Card>
                 <EmployeeForm />
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind( this )}>
                         Save Changes
                     </Button>
                 </CardSection>
             </Card>
-        )
+        );
     }
 }
 
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+    return { name, phone, shift };
+};
 
-export default connect()(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
