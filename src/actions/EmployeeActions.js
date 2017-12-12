fb361f5a2f68ebfd3b36fb132b338455929da573
Kylie Stewart <kylie.stewart@me.com>
2017-12-11 18:48:33 -0700

    Handle reset & reduxified employeeSave

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
     EMPLOYEE_CREATE,
-    EMPLOYEE_FETCH_SUCCESS
+    EMPLOYEE_FETCH_SUCCESS,
+    EMPLOYEE_SAVE_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -36,6 +37,19 @@ export const employeesFetch = () => {
                     type: EMPLOYEE_FETCH_SUCCESS, 
                     payload: snapshot.val() 
                 });
+            });
+    };
+}
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+            .set({ name, phone, shift })
+            .then( () => {
+                dispatch({ type: EMPLOYEE_SAVE_SUCCESS })
+                Actions.main({ type: 'reset' })
             });
     };
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -7,3 +7,4 @@ export const LOGIN_USER = 'login_user';
 export const EMPLOYEE_UPDATE = 'employee_update';
 export const EMPLOYEE_CREATE = 'exployee_create';
 export const EMPLOYEE_FETCH_SUCCESS = 'employee_fetch_success';
+export const EMPLOYEE_SAVE_SUCCESS = 'employee_save_success';
