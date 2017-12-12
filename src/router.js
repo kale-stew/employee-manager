deba835dbed4bbe41bb71544de0823e77962ef17
Kylie Stewart <kylie.stewart@me.com>
2017-12-11 18:25:30 -0700

    new EmployeeEdit screen

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
     return (
@@ -21,7 +22,18 @@ const RouterComponent = () => {
                         rightTitle="Add New"
                         onRight={ () => Actions.employeeCreate() }
                     />
-                    <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" hideNavBar={false}/>
+                    <Scene 
+                        key="employeeCreate" 
+                        component={EmployeeCreate} 
+                        title="Create Employee" 
+                        hideNavBar={false}
+                    />
+                    <Scene 
+                        key="employeeEdit"
+                        component={EmployeeEdit}
+                        title="Edit Employee"
+                        hideNavBar={false}
+                    />
                 </Scene>
             </Scene>
         </Router>
